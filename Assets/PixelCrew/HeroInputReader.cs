b70f53523b9e3360257f82cacf67eae8b005bbc4
Kotobozia <Kotobozia@users.noreply.github.com>
2022-06-18 21:34:34 +0300

    Lesson 4.1 - movement (part I)
    Watched and followed the stream up to 54:20

---
+++ Assets/PixelCrew/HeroInputReader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField] private HeroScript _hero;
+    
+    private void Awake()
+    {
+        _hero = GetComponent<HeroScript>();
+    }
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        _hero.SetDirection(horizontal);
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            _hero.SaySomething();
+        }
+    }
+}
